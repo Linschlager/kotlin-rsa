4f6845f154ae787b9e1781dba9c1bafd3f09554c
Linus Vettiger <linus4123@gmail.com>
2019-04-09 16:27:33 +0200

    Implemented file reader

--- src/FileReader.kt
+++ src/FileReader.kt
@@ -1,9 +1,13 @@
 import java.io.File
 
 fun readPrivateKey(): String {
-    var prk = ""
-    File("sk.txt").forEachLine {
-        prk = it
-    }
-    return prk
+    // Read the input file
+    // TODO make input file dynamic
+    val lines = File("input/sk.txt").readLines()
+    // Since only one line actually contains input, only use the first line
+    var firstLine = lines.first()
+    // Remove first and last bracket if present
+    if (firstLine.startsWith('(')) firstLine = firstLine.substring(1)
+    if (firstLine.endsWith(')')) firstLine = firstLine.substring(0, firstLine.length-1)
+    return firstLine
 }
--- test/FileReaderTest.kt
+++ test/FileReaderTest.kt
@@ -4,5 +4,7 @@ class FileReaderTest {
     @Test fun `can read file`() {
         val pk: String = readPrivateKey()
         assert(pk.isNotEmpty())
+        assert(!pk.contains('('))
+        assert(!pk.contains(')'))
     }
 }
