411d3e704cdaa3e89b064d6dec9f0e7a4d7afea6
Linus Vettiger <linus4123@gmail.com>
2019-04-09 17:46:34 +0200

    Implemented EEA

--- test/MathLibTest.kt
+++ test/MathLibTest.kt
@@ -1,8 +1,12 @@
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
 
 class MathLibTest {
     @Test fun `runs the extended euclidean algorithm correctly`() {
-        assert(false)
+        assertEquals(EEAResult(1.toBigInteger(), 2.toBigInteger(), 13.toBigInteger()), eea(18.toBigInteger(), 7.toBigInteger()))
+        assertNotEquals(EEAResult(2.toBigInteger(), 2.toBigInteger(), 13.toBigInteger()), eea(18.toBigInteger(), 7.toBigInteger()))
+        assertEquals(EEAResult(1.toBigInteger(), 13.toBigInteger(), 2.toBigInteger()), eea(7.toBigInteger(), 18.toBigInteger()))
     }
 
     @Test fun `runs the fast exponentiation algorithm correctly`() {
