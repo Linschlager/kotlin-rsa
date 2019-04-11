d130930326d66f56b109a2513b918e24850888e3
Linus Vettiger <linus.vettiger@novartis.com>
2019-04-11 15:15:23 +0200

    Implemented RSA keygen, encrypt and decrypt

--- src/ch/linusvettiger/kotlinrsa/RSAImplementation.kt
+++ src/ch/linusvettiger/kotlinrsa/RSAImplementation.kt
@@ -1,2 +1,39 @@
 package ch.linusvettiger.kotlinrsa
 
+import java.math.BigInteger
+import java.security.InvalidAlgorithmParameterException
+
+class Keys(val n: BigInteger, val phi: BigInteger, val e: BigInteger, val d: BigInteger) {
+    override fun toString(): String {
+        return "φ(n): %d\nPublic Key: (%d, %d)\nPrivate Key: (%d, %d)".format(phi, n, e, n, d)
+    }
+}
+
+fun generateKeys(p: BigInteger, q: BigInteger): Keys {
+    if (!p.isProbablePrime(20) || !q.isProbablePrime(20) || p === q) {
+        throw InvalidAlgorithmParameterException("p and q have to be two distinct primes")
+    }
+
+
+    val n = p*q
+    val phi = (p-1.toBigInteger())*(q-1.toBigInteger())
+
+    val e = 65537.toBigInteger()
+
+    val d = (eea(phi, e)).y0
+
+    if ((e*d).rem(phi) != 1.toBigInteger()) {
+        error("Invalid KeyPair: " + (e*d).rem(phi))
+    }
+
+    //Test: val m = (fea(message.toBigInteger(), e*d, n))
+    return Keys(n, phi, e, d)
+}
+
+fun encrypt(message: BigInteger, k: Keys): BigInteger {
+    return fea(message, k.e, k.n)
+}
+
+fun decrypt(message: BigInteger, k: Keys): BigInteger {
+    return fea(message, k.d, k.n)
+}
--- test/ch/linusvettiger/kotlinrsa/RSAImplementationTest.kt
+++ test/ch/linusvettiger/kotlinrsa/RSAImplementationTest.kt
@@ -1,2 +1,40 @@
 package ch.linusvettiger.kotlinrsa
 
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.math.BigInteger
+import kotlin.test.assertEquals
+
+class RSAImplementationTest {
+    @Test fun `detects invalid p or q`() {
+        assertThrows<Exception> {
+            generateKeys(10.toBigInteger(), 20.toBigInteger())
+        }
+    }
+
+    @Test fun `generates valid keys`() {
+        val k = generateKeys(929.toBigInteger(), 619.toBigInteger())
+        assertEquals(BigInteger.ONE, (k.e*k.d).rem(k.phi))
+    }
+
+    @Test fun `can encrypt and decrypt a message`() {
+        val k = generateKeys(929.toBigInteger(), 619.toBigInteger())
+        val message = 100.toBigInteger()
+        val encryptedMessage = encrypt(message, k)
+        println("Encrypted Message: %d".format(encryptedMessage))
+        val decryptedMessage = decrypt(encryptedMessage, k)
+        println("Decrypted Message: %d".format(decryptedMessage))
+        assertEquals(message, decryptedMessage)
+    }
+
+    @Test fun `can encrypt and decrypt a big message`() {
+        // n has to be > message to not cause an overflow
+        val k = generateKeys(602521.toBigInteger(), 1226741.toBigInteger())
+        val message = "123456789".toBigInteger()
+        val encryptedMessage = encrypt(message, k)
+        println("Encrypted Message: %d".format(encryptedMessage))
+        val decryptedMessage = decrypt(encryptedMessage, k)
+        println("Decrypted Message: %d".format(decryptedMessage))
+        assertEquals(message, decryptedMessage)
+    }
+}
