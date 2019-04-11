412121835832d0b537b7852db91d6662115dc4e0
Linus Vettiger <linus.vettiger@novartis.com>
2019-04-11 11:10:56 +0200

    added additional test to validate input

--- test/ch/linusvettiger/kotlinrsa/ExtendedEuclideanAlgorithmTest.kt
+++ test/ch/linusvettiger/kotlinrsa/ExtendedEuclideanAlgorithmTest.kt
@@ -1,17 +1,20 @@
 package ch.linusvettiger.kotlinrsa
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.security.InvalidAlgorithmParameterException
 import kotlin.test.assertEquals
 import kotlin.test.assertNotEquals
 
-class MathLibTest {
-    @Test fun `runs the extended euclidean algorithm correctly`() {
+class ExtendedEuclideanAlgorithmTest {
+    @Test fun `runs the algorithm correctly`() {
         assertEquals(EEAResult(1.toBigInteger(), 2.toBigInteger(), 13.toBigInteger()), eea(18.toBigInteger(), 7.toBigInteger()))
         assertNotEquals(EEAResult(2.toBigInteger(), 2.toBigInteger(), 13.toBigInteger()), eea(18.toBigInteger(), 7.toBigInteger()))
         assertEquals(EEAResult(1.toBigInteger(), 13.toBigInteger(), 2.toBigInteger()), eea(7.toBigInteger(), 18.toBigInteger()))
     }
 
-    @Test fun `runs the fast exponentiation algorithm correctly`() {
-        assert(false)
+    @Test fun `cannot compute 0 values`() {
+        assertThrows<InvalidAlgorithmParameterException> { eea(1.toBigInteger(), 0.toBigInteger()) }
+        assertThrows<InvalidAlgorithmParameterException> { eea(0.toBigInteger(), 1.toBigInteger()) }
     }
 }
