ecf396aa795e362d241d95111f557ee522977a3f
Linus Vettiger <linus.vettiger@novartis.com>
2019-04-11 16:19:38 +0200

    Implemented main() function

--- src/ch/linusvettiger/kotlinrsa/Main.kt
+++ src/ch/linusvettiger/kotlinrsa/Main.kt
@@ -1,5 +1,19 @@
 package ch.linusvettiger.kotlinrsa
 
-fun main(args: Array<String>) {
-    System.out.println("Hello, world")
+import java.math.BigInteger
+
+// Put the whole thing together
+fun main() {
+    // Load the message
+    val message = readMessage()
+    // Load the secret file and map to BigIntegers
+    val keys = readKeyPair().map { it.toBigInteger() }
+    // Generate the Keys object with the known data
+    val k = Keys(keys[0], BigInteger.ZERO, BigInteger.ZERO, keys[1])
+    // Decrypt the input data
+    message.forEach {
+        val decryptedMessage = decrypt(it, k)
+        // Output the encrypted data and the keypair
+        print(decryptedMessage.toInt().toChar())
+    }
 }
