8c6521b992c5777dd24608ac3072dcf3cdc6a267
Linus Vettiger <linus.vettiger@novartis.com>
2019-04-12 10:29:56 +0200

    Added CLI functionality

--- src/ch/linusvettiger/kotlinrsa/Main.kt
+++ src/ch/linusvettiger/kotlinrsa/Main.kt
@@ -1,19 +1,91 @@
 package ch.linusvettiger.kotlinrsa
 
 import java.math.BigInteger
+import kotlin.random.Random
 
-// Put the whole thing together
-fun main() {
-    // Load the message
-    val message = readMessage()
-    // Load the secret file and map to BigIntegers
-    val keys = readKeyPair().map { it.toBigInteger() }
-    // Generate the Keys object with the known data
-    val k = Keys(keys[0], BigInteger.ZERO, BigInteger.ZERO, keys[1])
-    // Decrypt the input data
-    message.forEach {
-        val decryptedMessage = decrypt(it, k)
-        // Output the encrypted data and the keypair
-        print(decryptedMessage.toInt().toChar())
+// Implement the outside interface
+fun main(args: Array<String>) {
+    if (args.isEmpty()) {
+        println("Usage:")
+        println("--file=<path-to-file> File to encrypt/decrypt. Format: Comma separated characters")
+        println("--private-key=<path-to-file> File containing the private key. Format (n,d)")
+        println("--public-key=<path-to-file> File containing the public key. Format: (n,e)")
+        println("--encrypt Encrypt the input file")
+        println("--decrypt Decrypt the input file using --private-key")
+        error("No arguments provided")
     }
+
+    var inputFilePath = ""
+    var privateKeyPath = ""
+    var publicKeyPath = ""
+    var programMode = ""
+    args.forEach{
+        val parts = if (it.contains('=')) {
+            it.split('=')
+        } else {
+            listOf(it)
+        }
+        when {
+            parts[0] == "--file" -> inputFilePath = parts[1]
+            parts[0] == "--private-key" -> privateKeyPath = parts[1]
+            parts[0] == "--public-key" -> publicKeyPath = parts[1]
+            parts[0] == "--decrypt" -> programMode = "decrypt"
+            parts[0] == "--encrypt" -> programMode = "encrypt"
+        }
+    }
+
+    // Load the encrypted message
+    val message = readMessage(inputFilePath)
+    var keys = Keys(BigInteger.ZERO, BigInteger.ZERO, BigInteger.ZERO, BigInteger.ZERO) // Default initialisation
+
+    if (inputFilePath.isBlank()) {
+        error("Please pass the input using --file")
+    }
+    if (programMode == "decrypt" && privateKeyPath.isBlank()) {
+        error("To decrypt the data, you need to define the path to a private key file using --private-key")
+    }
+    if (!publicKeyPath.isBlank()) {
+        val inputKeys = readKeyPair(publicKeyPath).map { it.toBigInteger() }
+        keys = Keys(inputKeys[0], BigInteger.ZERO, inputKeys[1], BigInteger.ZERO)
+    }
+    if (!privateKeyPath.isBlank()) {
+        val inputKeys = readKeyPair(privateKeyPath).map { it.toBigInteger() }
+        keys = Keys(inputKeys[0], BigInteger.ZERO, BigInteger.ZERO, inputKeys[1])
+    }
+    if (publicKeyPath.isBlank() && privateKeyPath.isBlank()) {
+        var p: BigInteger = BigInteger.ZERO
+        var q: BigInteger = BigInteger.ZERO
+
+        while (!p.isProbablePrime(1024)) {
+            p = Random.nextLong(0, Long.MAX_VALUE).toBigInteger()
+        }
+        while (!q.isProbablePrime(1024)) {
+            q = Random.nextLong(0, Long.MAX_VALUE).toBigInteger()
+        }
+        keys = generateKeys(p, q)
+    }
+    if (programMode == "decrypt") {
+        // Decrypt the input data
+        message.forEach {
+            val decryptedMessage = decrypt(it.toBigInteger(), keys)
+            // Output the encrypted data and the keypair
+            print(decryptedMessage.toInt().toChar())
+        }
+    }
+    if (programMode == "encrypt") {
+        message.forEach { single ->
+            // If the file contains a
+            single.forEach {
+                print(encrypt(it.toInt().toBigInteger(), keys))
+                print(",")
+            }
+        }
+    }
+    println()
+    println("Key used: ")
+    // Only output the actually set values
+    if (keys.e > BigInteger.ZERO)
+        println("Public Key (n, e) = (%d, %d)".format(keys.n, keys.e))
+    if (keys.d > BigInteger.ZERO)
+        println("Private Key (n, d) = (%d, %d)".format(keys.n, keys.d))
 }
